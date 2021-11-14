6bc75d2d61cd1bdf2e4ef69ccc80f2c747d3346b
Ayanbek <aianbek.01@mail.ru>
2021-11-14 16:44:54 +0600

    Go

--- app/sales-api/handlers/handlers.go
+++ app/sales-api/handlers/handlers.go
@@ -1,7 +1,10 @@
 package handlers
 
 import (
+	"github.com/jmoiron/sqlx"
 	"gitlab.com/FireH24d/business/auth"
+	"gitlab.com/FireH24d/business/data/product"
+	"gitlab.com/FireH24d/business/data/user"
 	"gitlab.com/FireH24d/business/middleware"
 	"log"
 	"net/http"
@@ -12,17 +15,39 @@ import (
 
 // API constructs an http.Handler with all application routes defined.
 
-func API(build string, shutdown chan os.Signal, log *log.Logger, a *auth.Auth) *web.App {
+func API(build string, shutdown chan os.Signal, log *log.Logger, a *auth.Auth, db *sqlx.DB) *web.App {
 	// Construct the web.App which holds all routes as well as common Middleware.
 
 	app := web.NewApp(shutdown, middleware.Logger(log), middleware.Errors(log), middleware.Metrics(), middleware.Panics(log))
 	// Register debug check endpoints.
 
-	check := check{
-		log: log,
+	cg := checkGroup{
+		build: build,
+		db:    db,
 	}
 
-	app.Handle(http.MethodGet, "/readiness", check.readiness, middleware.Authenticate(a), middleware.Authorize(auth.RoleAdmin))
+	app.Handle(http.MethodGet, "/readiness", cg.readiness)
+	app.Handle(http.MethodGet, "/liveness", cg.liveness)
+	ug := userGroup{
+		user: user.New(log, db),
+		auth: a,
+	}
+	app.Handle(http.MethodGet, "/users/:page/:rows", ug.query, middleware.Authenticate(a), middleware.Authorize(auth.RoleAdmin))
+	app.Handle(http.MethodGet, "/users/token/:kid", ug.token)
+	app.Handle(http.MethodGet, "/users/:id", ug.queryByID, middleware.Authenticate(a))
+	app.Handle(http.MethodPost, "/users", ug.create, middleware.Authenticate(a), middleware.Authorize(auth.RoleAdmin))
+	app.Handle(http.MethodPut, "/users/:id", ug.update, middleware.Authenticate(a), middleware.Authorize(auth.RoleAdmin))
+	app.Handle(http.MethodDelete, "/users/:id", ug.delete, middleware.Authenticate(a), middleware.Authorize(auth.RoleAdmin))
+
+	// Register product and sale endpoints.
+	pg := productGroup{
+		product: product.New(log, db),
+	}
+	app.Handle(http.MethodGet, "/v1/products/:page/:rows", pg.query, middleware.Authenticate(a))
+	app.Handle(http.MethodGet, "/v1/products/:id", pg.queryByID, middleware.Authenticate(a))
+	app.Handle(http.MethodPost, "/v1/products", pg.create, middleware.Authenticate(a))
+	app.Handle(http.MethodPut, "/v1/products/:id", pg.update, middleware.Authenticate(a))
+	app.Handle(http.MethodDelete, "/v1/products/:id", pg.delete, middleware.Authenticate(a))
 
 	return app
 }
--- go.mod
+++ go.mod
@@ -4,13 +4,20 @@ go 1.16
 
 require (
 	github.com/ardanlabs/conf v1.3.3
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/dgrijalva/jwt-go/v4 v4.0.0-preview1
 	github.com/dimfeld/httptreemux/v5 v5.2.2
+	github.com/dimiro1/darwin v0.0.0-20191008194338-370f81775d3b
 	github.com/go-playground/locales v0.13.0
 	github.com/go-playground/universal-translator v0.17.0
+	github.com/google/go-cmp v0.5.6
 	github.com/google/uuid v1.1.4
+	github.com/jmoiron/sqlx v1.3.4
 	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/lib/pq v1.10.4
 	github.com/pkg/errors v0.9.1
+	go.opentelemetry.io/otel/trace v1.2.0
+	golang.org/x/crypto v0.0.0-20211108221036-ceb1ce70b4fa
 	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
 	gopkg.in/go-playground/validator.v9 v9.31.0
 )
